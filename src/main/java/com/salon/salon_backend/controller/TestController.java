61fee409517c7d8bbd6693266986027ee2a41aec
Kanhaiya211 <kanhaiyadhongade14@gmail.com>
2026-06-23 00:27:30 +0530

    Refactor TestController to include mail username

--- src/main/java/com/salon/salon_backend/controller/TestController.java
+++ src/main/java/com/salon/salon_backend/controller/TestController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/api/test")
-    public String test() {
+    @Value("${MAIL_USERNAME}")
+    private String username;
 
-        return "Protected API Working";
+    @GetMapping("/mail")
+    public String test() {
+        return username;
     }
 }
