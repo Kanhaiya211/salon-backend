13bcd11c1a7119777d03398b641d61606617bdee
Kanhaiya211 <kanhaiyadhongade14@gmail.com>
2026-06-23 15:39:09 +0530

    Add existsByEmail method to UserRepository

--- src/main/java/com/salon/salon_backend/repository/UserRepository.java
+++ src/main/java/com/salon/salon_backend/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.salon.salon_backend.repository;
-import 
- org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salon.salon_backend.entity.User;
 
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salon.salon_backend.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
+    boolean existsByEmail(String email);
 }
